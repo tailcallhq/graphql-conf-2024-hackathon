5de61fb8edcf8675afdba5acdbc85e854833084c
laststylebender <ran.mahadik14@gmail.com>
2024-09-05 21:13:11 +0530

    - clean up

--- mock-api/src/lib.rs
+++ mock-api/src/lib.rs
@@ -1,14 +1,42 @@
-use std::collections::HashMap;
-
 use axum::{http::StatusCode, response::IntoResponse};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 pub mod routes;
 
-#[derive(Default)]
 pub struct AppState {
-    pub users: HashMap<i64, UserData>,
-    pub posts: HashMap<String, PostData>,
+    // pub users: HashMap<i64, UserData>,
+    // pub posts: HashMap<String, PostData>,
+    pub user_template: serde_json::Value,
+    pub post_template: serde_json::Value,
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        Self {
+            user_template: json!({
+                    "id": "@Number|1~10",
+                    "name": "@Name",
+                    "username": "@FirstName",
+                    "email": "@Email",
+                    "phone": "@Phone",
+                    "website": "@Url",
+                    "address": {
+                        "zipcode": "@Zip",
+                        "geo": {
+                            "lat": "@Float",
+                            "lon": "@Float",
+                        }
+                    }
+            }),
+            post_template: json!({
+                "id": "@Number|1~10",
+                "userId": "@Number|1~10",
+                "title": "@Title",
+                "body": "@Sentence",
+            }),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -37,7 +65,7 @@ pub struct GeoData {
 #[derive(Clone, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct PostData {
-    pub id: String,
+    pub id: i64,
     pub title: String,
     pub user_id: i64,
     pub body: String,
--- mock-api/src/routes/get_post.rs
+++ mock-api/src/routes/get_post.rs
@@ -6,23 +6,15 @@ use axum::{
     Json,
 };
 use mock_json::mock;
-use serde_json::json;
 
 use crate::{AppError, AppState, PostData};
 
 pub async fn handle(
-    _state: State<Arc<AppState>>,
-    post_id: Path<String>,
+    state: State<Arc<AppState>>,
+    post_id: Path<i64>,
 ) -> Result<impl IntoResponse, AppError> {
     let post_id = post_id.0;
-    let template = json!({
-        "id": post_id,
-        "userId": "@Number|1~10",
-        "title": "@Title",
-        "body": "@Sentence",
-    });
-
-    let mocked_value = mock(&template).to_string();
-    let mocked_post_data: PostData = serde_json::from_str(&mocked_value)?;
-    Ok(Json(mocked_post_data))
+    let mut post: PostData = serde_json::from_str(&mock(&state.post_template).to_string())?;
+    post.id = post_id;
+    Ok(Json(post))
 }
