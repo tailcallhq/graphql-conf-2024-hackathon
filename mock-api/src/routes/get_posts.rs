8defaee7c369ca3dc8219389d23f19bdde0a374f
laststylebender <ran.mahadik14@gmail.com>
2024-09-05 21:24:31 +0530

    - implemented get users/posts route.

--- mock-api/src/routes/get_posts.rs
+++ mock-api/src/routes/get_posts.rs
@@ -1,10 +1,18 @@
 use std::sync::Arc;
 
-use axum::{extract::State, response::IntoResponse};
+use axum::{extract::State, response::IntoResponse, Json};
+use mock_json::mock;
 
-use crate::{AppError, AppState};
+use crate::{AppError, AppState, PostData};
 
-pub async fn handle(_state: State<Arc<AppState>>) -> Result<impl IntoResponse, AppError> {
-    // TODO: fill logic
-    Ok(())
+pub async fn handle(state: State<Arc<AppState>>) -> Result<impl IntoResponse, AppError> {
+    let posts: Vec<PostData> = (1..=10)
+        .map(|id| {
+            let mut post: PostData = serde_json::from_str(&mock(&state.post_template).to_string())?;
+            post.id = id;
+            Ok(post)
+        })
+        .collect::<Result<_, AppError>>()?;
+
+    Ok(Json(posts))
 }
