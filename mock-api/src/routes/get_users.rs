0ebc80b4431cbfb3970d47f8d9664be58e46aaca
laststylebender <ran.mahadik14@gmail.com>
2024-09-09 18:00:55 +0530

    - implemented batch end point for users.

--- mock-api/src/routes/get_users.rs
+++ mock-api/src/routes/get_users.rs
@@ -1,9 +1,25 @@
 use std::sync::Arc;
 
-use axum::{extract::State, response::IntoResponse, Json};
+use axum::{
+    extract::{Query, State},
+    response::IntoResponse,
+    Json,
+};
 
 use crate::{AppError, AppState};
 
-pub async fn handle(state: State<Arc<AppState>>) -> Result<impl IntoResponse, AppError> {
-    Ok(Json(state.db.users()))
+pub async fn handle(
+    state: State<Arc<AppState>>,
+    Query(params): Query<Vec<(String, i64)>>,
+) -> Result<impl IntoResponse, AppError> {
+    let users = if params.is_empty() {
+        state.db.users()
+    } else {
+        params.into_iter()
+            .filter(|(key, _)| key == "id")
+            .filter_map(|(_, id)| state.db.user(id))
+            .collect()
+    };
+
+    Ok(Json(users))
 }
