dc83fdb51a8f27700f97e8e4179c9b7234e3e625
dekkku <abhigaikwad147@gmail.com>
2024-09-11 04:55:22 +0530

    exploit reset property of mock server

--- projects/dekkku/run.sh
+++ projects/dekkku/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cargo run -p dekkku
+cargo run -p dekkku --release
--- projects/dekkku/src/main.rs
+++ projects/dekkku/src/main.rs
@@ -3,11 +3,31 @@ use async_graphql::http::GraphiQLSource;
 use async_graphql::*;
 use async_graphql_poem::*;
 use poem::{listener::TcpListener, web::Html, *};
-use reqwest::Client;
+use reqwest::{Client, ClientBuilder};
 use serde::{Deserialize, Serialize};
-use std::{sync::Arc, time::Duration};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 const BASE_URL: &str = "http://localhost:3000";
+
+struct Store {
+    post: Mutex<Post>,
+    users: Mutex<HashMap<i32, User>>,
+    is_post_same: Mutex<bool>,
+}
+
+impl Default for Store {
+    fn default() -> Self {
+        Self {
+            post: Mutex::new(Post::default()),
+            users: Mutex::new(HashMap::default()),
+            is_post_same: Mutex::new(false),
+        }
+    }
+}
 
 struct Query;
 
@@ -18,8 +38,30 @@ impl Query {
         ctx: &Context<'_>,
     ) -> std::result::Result<Vec<Post>, async_graphql::Error> {
         let client = ctx.data_unchecked::<Arc<Client>>();
+
         let response = client.get(format!("{}/posts", BASE_URL)).send().await?;
         let posts: Vec<Post> = serde_json::from_value(response.json().await?)?;
+
+        // exploit the `reset` property of Mock Server.
+
+        let store = ctx.data_unchecked::<Arc<Store>>();
+
+        let is_same = {
+            let mut cached_post = store.post.lock().unwrap();
+            let is_same = (*cached_post) == posts[0];
+            if is_same {
+                true
+            } else {
+                *cached_post = posts[0].clone();
+                false
+            }
+        };
+
+        {
+            let mut is_post_same = store.is_post_same.lock().unwrap();
+            *is_post_same = is_same;
+        }
+
         Ok(posts)
     }
 
@@ -48,13 +90,14 @@ impl Query {
         ctx: &Context<'_>,
         id: i32,
     ) -> std::result::Result<Option<User>, async_graphql::Error> {
+        // TODO: cache the user for future purpose.
         let loader = ctx.data_unchecked::<DataLoader<UserLoader>>();
         let user = loader.load_one(id).await?;
         Ok(user)
     }
 }
 
-#[derive(SimpleObject, Serialize, Deserialize, Debug, Clone)]
+#[derive(SimpleObject, Serialize, Deserialize, Debug, Clone, PartialEq, Default)]
 #[serde(rename_all = "camelCase")]
 #[graphql(complex)]
 struct Post {
@@ -71,9 +114,19 @@ impl Post {
         &self,
         ctx: &Context<'_>,
     ) -> std::result::Result<Option<User>, async_graphql::Error> {
-        let loader = ctx.data_unchecked::<DataLoader<UserLoader>>();
-        let user = loader.load_one(self.user_id).await?;
-        Ok(user)
+        let store = ctx.data_unchecked::<Arc<Store>>();
+        if *store.is_post_same.lock().unwrap() {
+            let user = store.users.lock().unwrap().get(&self.user_id).cloned();
+            Ok(user)
+        } else {
+            let loader = ctx.data_unchecked::<DataLoader<UserLoader>>();
+            let user = loader.load_one(self.user_id).await?;
+            if let Some(actual_user) = user.as_ref() {
+                let mut user_store = store.users.lock().unwrap();
+                user_store.insert(self.user_id, actual_user.clone());
+            }
+            Ok(user)
+        }
     }
 }
 
@@ -162,6 +215,7 @@ fn create_schema() -> Schema<Query, EmptyMutation, EmptySubscription> {
         .data(client)
         .data(user_loader)
         .data(post_loader)
+        .data(Arc::new(Store::default()))
         .finish()
 }
 
@@ -183,3 +237,7 @@ async fn main() -> anyhow::Result<()> {
         .await?;
     Ok(())
 }
+
+// Ideas
+// 1. Connection Pooling.
+// 2. Caching based on POST's.
