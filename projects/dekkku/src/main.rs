7f90bd87baf9071684b4e5c4e2aac55d0dc838bd
dekkku <abhigaikwad147@gmail.com>
2024-09-12 11:02:49 +0530

    batch concurrent requests into one.

--- projects/dekkku/src/main.rs
+++ projects/dekkku/src/main.rs
@@ -2,6 +2,8 @@ use async_graphql::dataloader::*;
 use async_graphql::http::GraphiQLSource;
 use async_graphql::*;
 use async_graphql_poem::*;
+use bytes::Bytes;
+use dashmap::DashMap;
 use poem::{listener::TcpListener, web::Html, *};
 use rand::seq::SliceRandom;
 use reqwest::Client;
@@ -11,13 +13,16 @@ use std::{
     sync::{Arc, RwLock},
     time::Duration,
 };
+use tokio::sync::oneshot;
+use tokio_retry::strategy::{jitter, ExponentialBackoff};
+use tokio_retry::Retry;
 
 const BASE_URL: &str = "http://localhost:3000";
 const ALL_USERS: &str = "http://localhost:3000/users";
 const ALL_POSTS: &str = "http://localhost:3000/posts";
 
 struct Store {
-    post: RwLock<HashMap<i32, Post>>,
+    posts: RwLock<HashMap<i32, Post>>,
     users: RwLock<HashMap<i32, User>>,
     is_dirty: RwLock<bool>,
 }
@@ -25,7 +30,7 @@ struct Store {
 impl Default for Store {
     fn default() -> Self {
         Self {
-            post: RwLock::new(HashMap::new()),
+            posts: RwLock::new(HashMap::new()),
             users: RwLock::new(HashMap::new()),
             is_dirty: RwLock::new(false),
         }
@@ -40,31 +45,30 @@ impl Query {
         &self,
         ctx: &Context<'_>,
     ) -> std::result::Result<Vec<Post>, async_graphql::Error> {
-        let client = ctx.data_unchecked::<Arc<Client>>();
-        let response = client.get(ALL_POSTS).send().await?;
-        let posts: Vec<Post> = response.json().await?;
+        let http_client = ctx.data_unchecked::<Arc<RequestBatcher>>();
+        let posts: Vec<Post> = http_client.request(ALL_POSTS).await?;
         let store = ctx.data_unchecked::<Arc<Store>>();
 
         let are_posts_same = {
-            let is_cache_empty = store.post.read().unwrap().is_empty();
+            let is_cache_empty = store.posts.read().unwrap().is_empty();
             if is_cache_empty {
                 let mut rng = rand::thread_rng();
                 let selected_posts: Vec<&Post> = posts.choose_multiple(&mut rng, 2).collect();
                 if selected_posts.len() == 2 {
-                    let mut posts_writer = store.post.write().unwrap();
+                    let mut posts_writer = store.posts.write().unwrap();
                     for post in selected_posts {
-                        posts_writer.insert(post.id.unwrap(), post.clone());
+                        posts_writer.insert(post.id, post.clone());
                     }
                 }
                 false
             } else {
                 let cached_posts: Vec<Post> =
-                    store.post.read().unwrap().values().cloned().collect();
-                let mut posts_writer = store.post.write().unwrap();
+                    store.posts.read().unwrap().values().cloned().collect();
+                let mut posts_writer = store.posts.write().unwrap();
                 cached_posts.iter().all(|post| {
                     if let Some(new_post) = posts.iter().find(|p| p.id == post.id) {
                         if post != new_post {
-                            posts_writer.insert(new_post.id.unwrap(), new_post.clone());
+                            posts_writer.insert(new_post.id, new_post.clone());
                             false
                         } else {
                             true
@@ -95,7 +99,7 @@ impl Query {
         if let Some(actual_post) = post.as_ref() {
             let store = ctx.data_unchecked::<Arc<Store>>();
             let mut are_posts_same = true;
-            if let Some(cached_post) = store.post.read().unwrap().get(&id) {
+            if let Some(cached_post) = store.posts.read().unwrap().get(&id) {
                 if actual_post != cached_post {
                     *store.is_dirty.write().unwrap() = true;
                     are_posts_same = false;
@@ -105,7 +109,7 @@ impl Query {
                 // clean up the users.
                 store.users.write().unwrap().clear();
             }
-            store.post.write().unwrap().insert(id, actual_post.clone());
+            store.posts.write().unwrap().insert(id, actual_post.clone());
         }
         Ok(post)
     }
@@ -114,9 +118,8 @@ impl Query {
         &self,
         ctx: &Context<'_>,
     ) -> std::result::Result<Vec<User>, async_graphql::Error> {
-        let client = ctx.data_unchecked::<Arc<Client>>();
-        let response = client.get(ALL_USERS).send().await?;
-        let users: Vec<User> = response.json().await?;
+        let http_client = ctx.data_unchecked::<Arc<RequestBatcher>>();
+        let users: Vec<User> = http_client.request(ALL_USERS).await?;
 
         let store = ctx.data_unchecked::<Arc<Store>>();
         let mut rng = rand::thread_rng();
@@ -125,13 +128,9 @@ impl Query {
         if selected_users.len() == 2 {
             let users_writer = store.users.read().unwrap();
             let are_users_same = selected_users.iter().all(|user| {
-                if let Some(id) = user.id {
-                    users_writer
-                        .get(&id)
-                        .map_or(false, |cached_user| cached_user == *user)
-                } else {
-                    false
-                }
+                users_writer
+                    .get(&user.id)
+                    .map_or(false, |cached_user| cached_user == *user)
             });
 
             if !are_users_same {
@@ -166,7 +165,7 @@ impl Query {
 #[serde(rename_all = "camelCase")]
 #[graphql(complex)]
 struct Post {
-    id: Option<i32>,
+    id: i32,
     #[graphql(name = "userId")]
     user_id: i32,
     title: Option<String>,
@@ -208,7 +207,7 @@ impl Post {
 
 #[derive(SimpleObject, Serialize, Deserialize, Debug, Clone, PartialEq)]
 struct User {
-    id: Option<i32>,
+    id: i32,
     name: Option<String>,
     username: Option<String>,
     email: Option<String>,
@@ -229,8 +228,8 @@ struct Geo {
     lng: Option<f64>,
 }
 
-struct PostLoader(Arc<Client>);
-struct UserLoader(Arc<Client>);
+struct PostLoader(Arc<RequestBatcher>);
+struct UserLoader(Arc<RequestBatcher>);
 
 impl Loader<i32> for PostLoader {
     type Value = Post;
@@ -243,11 +242,8 @@ impl Loader<i32> for PostLoader {
         let mut result = std::collections::HashMap::new();
         for &id in keys {
             let url = format!("{}/posts/{}", BASE_URL, id);
-            let response = self.0.get(url).send().await?;
-            if response.status().is_success() {
-                let post: Post = response.json().await?;
-                result.insert(id, post);
-            }
+            let post: Post = self.0.request(&url).await?;
+            result.insert(id, post);
         }
         Ok(result)
     }
@@ -268,29 +264,114 @@ impl Loader<i32> for UserLoader {
             .collect::<Vec<_>>()
             .join("&");
         let url = format!("{}/users?{}", BASE_URL, qp);
-        let response = self.0.get(url).send().await?;
-        if response.status().is_success() {
-            let users: Vec<User> = response.json().await?;
-            for user in users {
-                if let Some(id) = user.id {
-                    result.insert(id, user);
-                }
-            }
+        let users: Vec<User> = self.0.request(&url).await?;
+        for user in users {
+            result.insert(user.id, user);
         }
         Ok(result)
     }
 }
 
+async fn fetch_with_retry(
+    client: &Client,
+    url: &str,
+) -> std::result::Result<reqwest::Response, reqwest::Error> {
+    let retry_strategy = ExponentialBackoff::from_millis(10).map(jitter).take(3); // Retry up to 3 times
+
+    Retry::spawn(retry_strategy, || client.get(url).send()).await
+}
+
+#[allow(dead_code)]
+struct HttpClient {
+    client: Arc<Client>,
+}
+
+impl HttpClient {
+    fn new(client: Arc<Client>) -> Self {
+        Self { client }
+    }
+
+    #[allow(dead_code)]
+    async fn request<T: for<'de> Deserialize<'de>>(
+        &self,
+        url: &str,
+    ) -> std::result::Result<T, anyhow::Error> {
+        let response = fetch_with_retry(&self.client, url).await?;
+        Ok(response.json().await?)
+    }
+}
+
+struct RequestBatcher {
+    client: Arc<Client>,
+    on_going_req:
+        Arc<DashMap<String, Vec<oneshot::Sender<std::result::Result<Bytes, anyhow::Error>>>>>,
+}
+
+impl RequestBatcher {
+    fn new(client: Arc<Client>) -> Self {
+        Self {
+            client,
+            on_going_req: Default::default(),
+        }
+    }
+
+    async fn request<T: for<'de> Deserialize<'de>>(
+        &self,
+        url: &str,
+    ) -> std::result::Result<T, anyhow::Error> {
+        let (tx, rx) = oneshot::channel();
+
+        let should_fetch = {
+            let mut entry = self.on_going_req.entry(url.to_owned()).or_default();
+            entry.push(tx);
+            entry.len() == 1
+        };
+
+        if should_fetch {
+            let client = self.client.clone();
+            let url = url.to_owned();
+            let on_going_req = self.on_going_req.clone();
+
+            tokio::spawn(async move {
+                let result = fetch_with_retry(&client, &url).await;
+
+                if let Ok(response) = result {
+                    if let Ok(body) = response.bytes().await {
+                        // let's say in queue, we've got around 10k requests and only one is executing right now.
+                        // and if it fails then we won't reach here to remove the all requests from queue
+                        // handle that scenario.
+                        let senders = { on_going_req.remove(&url) };
+                        if let Some((_, senders)) = senders {
+                            for sender in senders {
+                                let _ = sender.send(Ok(body.clone()));
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        let response = rx.await??;
+
+        Ok(serde_json::from_slice(&response)?)
+    }
+}
+
 fn create_schema() -> Schema<Query, EmptyMutation, EmptySubscription> {
     let client = Arc::new(Client::new());
-    let user_loader =
-        DataLoader::new(UserLoader(client.clone()), tokio::spawn).delay(Duration::from_millis(1));
-    let post_loader =
-        DataLoader::new(PostLoader(client.clone()), tokio::spawn).delay(Duration::from_millis(5));
+    let http_client = Arc::new(HttpClient::new(client.clone()));
+    let batch_client = Arc::new(RequestBatcher::new(client.clone()));
+
+    let user_loader = DataLoader::new(UserLoader(batch_client.clone()), tokio::spawn)
+        .delay(Duration::from_millis(1));
+    let post_loader = DataLoader::new(PostLoader(batch_client.clone()), tokio::spawn)
+        .delay(Duration::from_millis(5));
+
     Schema::build(Query, EmptyMutation, EmptySubscription)
-        .data(client)
+        .data(http_client)
         .data(user_loader)
         .data(post_loader)
+        .data(batch_client)
         .data(Arc::new(Store::default()))
         .finish()
 }
