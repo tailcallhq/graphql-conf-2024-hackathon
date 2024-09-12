fff15f965b5b0f14aceb1f054b9e03bb75e73f63
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-09-12 16:56:33 -0400

    disable tracing

--- projects/ssddOnTop/src/main.rs
+++ projects/ssddOnTop/src/main.rs
@@ -1,5 +1,5 @@
 fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
+    // tracing_subscriber::fmt::init();
     let rt = tokio::runtime::Builder::new_multi_thread()
         .worker_threads(num_cpus::get())
         .enable_all()
